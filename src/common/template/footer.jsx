a5bdadc4bdb21ebd96056b8255f9911eb2302ff5
Augusto Scher <augusto.scher@resultadosdigitais.com.br>
2019-08-25 15:20:49 -0300

    fix endpoint address

---
+++ src/common/template/footer.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default props => (
+    <footer className='main-footer'> 
+        <strong> 
+            Copyright &copy; 2017
+            <a href='https://augustoscher.github.io/website/#/home' target='_blank'>Augusto Scher</a>.
+        </strong>
+    </footer>
+)
---
+++ src/consts.js
@@ -0,0 +1,4 @@
+export default {
+    API_URL: 'https://as-my-money-backend.herokuapp.com/api',
+    OAPI_URL: 'https://as-my-money-backend.herokuapp.com/oapi',
+}
