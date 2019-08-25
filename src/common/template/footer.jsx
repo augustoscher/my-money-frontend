eeaf049704fd30905997debbfcbd217e33bd9fce
Augusto Scher <augusto.scher@resultadosdigitais.com.br>
2019-08-25 16:05:08 -0300

    branding

--- src/common/template/footer.jsx
+++ src/common/template/footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default props => (
     <footer className='main-footer'> 
         <strong> 
-            Copyright &copy; 2017
+            Copyright &copy; 2019 
             <a href='https://augustoscher.github.io/website/#/home' target='_blank'>Augusto Scher</a>.
         </strong>
     </footer>
